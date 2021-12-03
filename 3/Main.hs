8b71dc74977216564a75aa6ea78e79d1062a8317
Erik Hesselink <ehesselink@palantir.com>
2021-12-03 10:39:47 +0100

    Day 3 part 2

--- 3/Main.hs
+++ 3/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.List (transpose, sortBy, group, sort)
+import Data.List (transpose, sortBy, group, sort, unfoldr)
 import Data.Ord (comparing)
 import Control.Arrow ((***))
 import Numeric (readInt)
@@ -9,15 +9,15 @@ import Data.Char (digitToInt)
 main :: IO ()
 main = do
   f <- readFile "input/3"
-  let cols = parse f
-      (gamma, epsilon) = computeGammaEpsilon cols
+  let rows = lines f
+      (gamma, epsilon) = computeGammaEpsilon (transpose rows)
       result = gamma * epsilon
   print result
+  let (o, c) = (oxygen rows, co2 rows)
+      result2 = parseBinary o * parseBinary c
+  print result2
 
 type Bit = Char
-
-parse :: String -> [[Bit]]
-parse = transpose . lines
 
 computeGammaEpsilon :: [[Bit]] -> (Int, Int)
 computeGammaEpsilon = (parseBinary *** parseBinary) . unzip . map commonUncommon
@@ -25,9 +25,24 @@ computeGammaEpsilon = (parseBinary *** parseBinary) . unzip . map commonUncommon
 commonUncommon :: [Bit] -> (Bit, Bit)
 commonUncommon = (\[b1, b2] -> (b1, b2))
                . map head
-               . sortBy (flip $ comparing length)
+               . sortBy (flip (comparing length <> compare))
                . group
                . sort
 
 parseBinary :: [Bit] -> Int
 parseBinary = fst . head . readInt 2 (`elem` "01") digitToInt
+
+oxygen :: [[Bit]] -> [Bit]
+oxygen = unfoldr (step fst)
+
+co2 :: [[Bit]] -> [Bit]
+co2 = unfoldr (step snd)
+
+step :: ((Bit, Bit) -> Bit) -> [[Bit]] -> Maybe (Bit, [[Bit]])
+step _  [] = Nothing
+step _  [[]] = Nothing
+step _  [r:row] = Just (r, [row])
+step cu rows =
+  let d = cu . commonUncommon . map head $ rows
+      left = filter ((== d) . head) rows
+  in Just (d, map tail left)
