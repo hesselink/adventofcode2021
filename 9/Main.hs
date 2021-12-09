7ac6cba16c0bb0184cca65ce83b9e012aefd34d8
Erik Hesselink <hesselink@gmail.com>
2021-12-09 21:10:04 +0100

    Day 9 part 2

--- 9/Main.hs
+++ 9/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromMaybe)
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.List (sort, group, sortBy)
 
 main :: IO ()
 main = do
@@ -9,14 +12,17 @@ main = do
       lowPoints = findLowPoints ds
       result = sum . map (+1) $ lowPoints
   print result
+  let depthMap = parseAsMap f
+      result2 = solve depthMap
+  print result2
 
 findLowPoints :: [[Int]] -> [Int]
 findLowPoints ds =
   let above = repeat 9 : ds
       below = tail ds ++ [repeat 9]
-      right = map ((++ [9]) . tail) ds
-      left = map (9:) ds
-  in catMaybes $ concat $ zipWith5 (zipWith5 isLowPoint) ds above below right left
+      right_ = map ((++ [9]) . tail) ds
+      left_ = map (9:) ds
+  in catMaybes $ concat $ zipWith5 (zipWith5 isLowPoint) ds above below right_ left_
 
 isLowPoint :: Int -> Int -> Int -> Int -> Int -> Maybe Int
 isLowPoint d a b r l =
@@ -31,3 +37,54 @@ zipWith5 _ _  _  [] _  _  = []
 zipWith5 _ _  _  _  [] _  = []
 zipWith5 _ _  _  _  _  [] = []
 zipWith5 f (a:as) (b:bs) (c:cs) (d:ds) (e:es) = f a b c d e : zipWith5 f as bs cs ds es
+
+type Point = (Int, Int)
+
+parseAsMap :: String -> Map Point Int
+parseAsMap
+  = Map.fromList
+  . concat
+  . zipWith (\y -> zipWith (\x c -> ((x, y), read [c])) [0..]) [0..]
+  . lines
+
+findBasin :: Map Point Int -> Point -> Maybe Point
+findBasin ds p =
+  let Just d = Map.lookup p ds
+      pu = up p
+      pd = down p
+      pl = left p
+      pr = right p
+      a = fromMaybe 9 $ Map.lookup pu ds
+      b = fromMaybe 9 $ Map.lookup pd ds
+      l = fromMaybe 9 $ Map.lookup pl ds
+      r = fromMaybe 9 $ Map.lookup pr ds
+      lowest = snd . head . sort $ [(a, pu), (b, pd), (l, pl), (r, pr)]
+  in if d < a && d < b && d < r && d < l
+     then Just p
+     else if d == 9
+     then Nothing
+     else findBasin ds lowest -- could have memoized if too slow
+
+up :: Point -> Point
+up (x, y) = (x, y - 1)
+
+down :: Point -> Point
+down (x, y) = (x, y + 1)
+
+left :: Point -> Point
+left (x, y) = (x - 1, y)
+
+right :: Point -> Point
+right (x, y) = (x + 1, y)
+
+solve :: Map Point Int -> Int
+solve m
+  = product
+  . take 3
+  . sortBy (flip compare)
+  . map length
+  . group
+  . sort
+  . catMaybes
+  . map (findBasin m)
+  . Map.keys $ m
