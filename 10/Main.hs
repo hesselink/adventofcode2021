2031e41c3b0a7c0a3e1f87dc92cd726c1e43761a
Erik Hesselink <ehesselink@palantir.com>
2021-12-10 11:48:26 +0100

    Day 10 part 2

--- 10/Main.hs
+++ 10/Main.hs
@@ -1,11 +1,15 @@
 module Main where
 
+import Data.List (sort)
+
 main :: IO ()
 main = do
   f <- readFile "input/10"
   let ls = lines f
       result = sum . map score $ ls
   print result
+  let result2 = calculateScore . filter (/= 0) . map score2 $ ls
+  print result2
 
 score :: String -> Int
 score = either errorToScore (const 0) . parse
@@ -17,9 +21,12 @@ data BraceType
   | Triangle
   deriving (Show, Eq)
 
-data Error = Mismatch BraceType
+data Error
+  = Mismatch BraceType
+  | Incomplete [BraceType]
 
 errorToScore :: Error -> Int
+errorToScore (Incomplete _) = 0 -- Ignore incomplete in part 1
 errorToScore (Mismatch bt) =
   case bt of
     Paren -> 3
@@ -30,7 +37,8 @@ errorToScore (Mismatch bt) =
 parse :: String -> Either Error ()
 parse = go []
   where
-    go _      [] = Right () -- ignore incomplete for now
+    go []      [] = Right ()
+    go stack   [] = Left (Incomplete stack)
     go (s:st) (p:ps) | isClose p = if braceType p /= s
                                     then Left (Mismatch (braceType p))
                                     else go st ps
@@ -47,3 +55,19 @@ braceType c =
   else if c `elem` "{}" then Curly
   else if c `elem` "<>" then Triangle
   else error $ "Unknown brace type: " ++ [c]
+
+score2 :: String -> Int
+score2 = either errorToScore2 (const 0) . parse
+
+errorToScore2 :: Error -> Int
+errorToScore2 (Incomplete st) = foldl step 0 st
+  where
+   step sc c = 5 * sc + case c of
+     Paren -> 1
+     Square -> 2
+     Curly -> 3
+     Triangle -> 4
+errorToScore2 (Mismatch _) = 0 -- Ignore mismatch in part 2
+
+calculateScore :: [Int] -> Int
+calculateScore ss = head . drop (length ss `div` 2) . sort $ ss
