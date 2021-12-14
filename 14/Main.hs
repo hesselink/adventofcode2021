ac37a38b5be6de607a02820aa4e678716e6ecd1a
Erik Hesselink <ehesselink@palantir.com>
2021-12-14 13:07:49 +0100

    Day 14

---
+++ 14/Main.hs
@@ -0,0 +1,80 @@
+module Main where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.List (foldl')
+import Data.Maybe (fromMaybe)
+
+import Lib
+
+main :: IO ()
+main = do
+  f <- readFile "input/14"
+  let st = parse f
+      finalCounts = Map.elems . countElements . nTimes 10 step $ st
+      result = maximum finalCounts - minimum finalCounts
+  print result
+  let finalCounts2 = Map.elems . countElements . nTimes 40 step $ st
+      result2 = maximum finalCounts2 - minimum finalCounts2
+  print result2
+
+data State = State
+  { counts :: Count Pair
+  , rules :: [Rule]
+  , initEl :: Char
+  , lastEl :: Char
+  } deriving (Show, Eq)
+
+type Count a = Map a Integer
+type Pair = (Char, Char) -- pair of elements AB
+type Rule = (Pair, (Pair, Pair))
+
+parse :: String -> State
+parse str = case lines str of
+  (tmpl:"":rls) -> State (parseTemplate tmpl) (parseRules rls) (head tmpl) (last tmpl)
+  _ -> error "Failed to parse"
+
+parseTemplate :: String -> Count Pair
+parseTemplate str = count $ zipWith (,) str (tail str)
+
+parseRules :: [String] -> [Rule]
+parseRules = map parseRule
+
+parseRule :: String -> (Pair, (Pair, Pair))
+parseRule (a:b:' ':'-':'>':' ':c:[]) = ((a, b), ((a, c), (c, b)))
+parseRule s = error $ "Failed to parse Rule: " ++ s
+
+count :: Ord a => [a] -> Count a
+count = foldl' (flip incCount) Map.empty
+
+fromCounts :: Ord a => [(a, Integer)] -> Count a
+fromCounts = unionsCounts . map (Map.fromList . pure)
+
+incCount :: Ord a => a -> Count a -> Count a
+incCount v = Map.insertWith (+) v 1
+
+getCount :: Ord a => a -> Count a -> Integer
+getCount v = fromMaybe 0 . Map.lookup v
+
+resetCount :: Ord a => a -> Count a -> Count a
+resetCount = Map.delete
+
+unionsCounts :: Ord a => [Count a] -> Count a
+unionsCounts = Map.unionsWith (+)
+
+step :: State -> State
+step st =
+  let cs = counts st
+      rs = rules st
+      newCs = map (split cs) rs
+      withoutChanged = foldl' (flip resetCount) cs (map fst rs)
+  in st { counts = unionsCounts (withoutChanged:newCs) }
+
+split :: Count Pair -> Rule -> Count Pair
+split cs (p, (p1, p2)) =
+  let c = getCount p cs
+  in if c == 0 then Map.empty else fromCounts [(p1, c), (p2, c)]
+
+countElements :: State -> Count Char
+countElements st =
+  fmap (`div` 2) . incCount (lastEl st) . incCount (initEl st) . unionsCounts . map (\((a,b), n) -> fromCounts [(a, n), (b, n)]) . Map.toList . counts $ st
