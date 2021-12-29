ac4083ce4921b3822565b8c18dd9c01567452fd1
Erik Hesselink <hesselink@gmail.com>
2021-12-29 12:53:32 +0100

    Day 21 part 2

--- 21/Main.hs
+++ 21/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Maybe (fromJust)
+import Control.Arrow ((&&&))
+import Data.List (group, sort, genericLength)
+
 main :: IO ()
 main = do
   f <- readFile "input/21"
   let st = parse f
       result = answer $ playUntilWin st
   print result
+  let result2 = fromJust $ Map.lookup (to2 st) allStates
+  print (max (wins1 result2) (wins2 result2))
 
 data State = State
   { pos1 :: Int
@@ -48,3 +56,62 @@ answer :: State -> Int
 answer st =
   let losingScore = if score1 st >= 1000 then score2 st else score1 st
   in losingScore * rolls st
+
+data State2 = State2
+  { pos1_ :: Int
+  , pos2_ :: Int
+  , score1_ :: Int
+  , score2_ :: Int
+  , turn1 :: Bool
+  } deriving (Eq, Ord, Show)
+
+to2 :: State -> State2
+to2 st = State2 (pos1 st) (pos2 st) (score1 st) (score2 st) True
+
+step2 :: Int -> State2 -> State2
+step2 mv st =
+  if turn1 st
+  then
+    let pos1' = move mv (pos1_ st)
+        score1' = score1_ st + pos1'
+    in st { pos1_ = pos1'
+          , score1_ = score1'
+          , turn1 = not (turn1 st)
+          }
+  else
+    let pos2' = move mv (pos2_ st)
+        score2' = score2_ st + pos2'
+    in st { pos2_ = pos2'
+          , score2_ = score2'
+          , turn1 = not (turn1 st)
+          }
+
+data Result = Result
+  { wins1 :: Integer
+  , wins2 :: Integer
+  } deriving (Show, Eq)
+
+instance Monoid Result where
+  mempty = Result 0 0
+
+instance Semigroup Result where
+  Result w1 w2 <> Result w1' w2' = Result (w1 + w1') (w2 + w2')
+
+allStates :: Map State2 Result
+allStates =
+  let m = Map.fromList [ (st, result st) | p1 <- [1..10], p2 <- [1..10], s1 <- [0..30], s2 <- [0..30], t1 <- [True, False], let st = State2 p1 p2 s1 s2 t1]
+      result st =
+        if score1_ st >= 21
+        then Result 1 0
+        else if score2_ st >= 21
+        then Result 0 1
+        else
+          foldMap (\(s, c) -> scale c . fromJust . flip Map.lookup m $ s)
+            [ (st', c) | (r, c) <- distribution , let st' = step2 r st ]
+  in m
+
+scale :: Integer -> Result -> Result
+scale c (Result w1 w2) = Result (w1 * c) (w2 * c)
+
+distribution :: [(Int, Integer)] -- sum, count
+distribution = map (head &&& genericLength) . group . sort $ [ r1 + r2 + r3 | r1 <- [1..3], r2 <- [1..3], r3 <- [1..3] ]
